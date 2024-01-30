0e145f99a4d7f65fe55958a7a09fc330b0407378
rouhani <majid.rouhani@ntnu.no>
2024-01-30 18:31:47 +0100

    initial version

---
+++ src/main/java/demo/parallel/Main.java
@@ -0,0 +1,12 @@
+package demo.parallel;
+
+public class Main {
+  /**
+   * Java main for when running without JavaFX launcher
+   *
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    MainApp.main(args);
+  }
+}
